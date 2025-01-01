d161c3a74ca2b102d789296f07b462389cdf05ed
fer.tsx <fernandocostadev98@gmail.com>
2025-01-01 15:19:36 -0300

    feat: add update challenge gql input

---
+++ packages/challenge-manager/src/core/challenge/infra/graphql/input/UpdateChallengeInput.ts
@@ -0,0 +1,11 @@
+import { Field, InputType } from "@nestjs/graphql";
+
+@InputType()
+export class UpdateChallengeInput {
+  @Field()
+  id: string;
+  @Field()
+  title: string;
+  @Field()
+  description: string;
+}
