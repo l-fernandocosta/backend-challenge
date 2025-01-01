56645883b0e3d7ab3a2c0e309b12aeba5150fa41
fer.tsx <fernandocostadev98@gmail.com>
2025-01-01 15:20:54 -0300

    feat: init appmodule

--- packages/challenge-manager/src/app.module.ts
+++ packages/challenge-manager/src/app.module.ts
@@ -1,8 +1,27 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
+import { GraphQLModule } from '@nestjs/graphql';
+import ChallengeModule from './core/challenge/infra/ChallengeModule';
+import { GraphqlExceptionFilter } from './shared/infra/interceptor/GraphQLExceptionFilter';
+import SubmissionModule from './core/submission/infra/SubmissionModule';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true, 
+      sortSchema: true, 
+      driver: ApolloDriver, 
+      playground: true, 
+    }), 
+    ChallengeModule, 
+    SubmissionModule, 
+  ],
+  providers: [
+    {
+      provide: APP_FILTER, 
+      useClass: GraphqlExceptionFilter
+    } 
+  ]
 })
 export class AppModule { }
